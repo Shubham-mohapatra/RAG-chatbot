c085aeb870efb91906b71a3d4d6dc5abdc496be9
Shubham Mohapatra <shubhammohapatra094@gmail.com>
2025-08-27 23:40:24 +0530

    Create simplified LangChain version to avoid Pydantic v1/v2 conflicts

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic_models import QueryInput, QueryResponse, DocumentInfo, DeleteFileRequest
-from langchain_utils import get_rag_chain
+from langchain_utils_simple import get_rag_chain, get_response_from_query
 from db_utils import insert_application_logs, get_chat_history, get_all_documents, insert_document_record, delete_document_record
 from chroma_utils import index_document_to_chroma, delete_doc_from_chroma
 import os
@@ -80,12 +80,9 @@ def chat(query_input: QueryInput):
 
     try:
         chat_history = get_chat_history(session_id)
-        rag_chain = get_rag_chain(query_input.model.value)
+        rag_chain = get_rag_chain(model_name=query_input.model.value)
         
-        answer = rag_chain.invoke({
-            "input": query_input.question,
-            "chat_history": chat_history
-        })['answer']
+        answer = get_response_from_query(rag_chain, query_input.question)
         
         insert_application_logs(session_id, query_input.question, answer, query_input.model.value)
         logging.info(f"Session ID: {session_id}, AI Response: {answer}")
