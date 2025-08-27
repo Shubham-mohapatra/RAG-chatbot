63490b141c802d6731fe5bebadc6bd5c6a2df91c
Shubham Mohapatra <shubhammohapatra094@gmail.com>
2025-08-27 22:28:44 +0530

    Update render config for faster deployment

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   extends: ['next/core-web-vitals'],
   rules: {
-    // Disable some strict rules for development
     '@next/next/no-img-element': 'off',
     'react-hooks/exhaustive-deps': 'warn',
   },
