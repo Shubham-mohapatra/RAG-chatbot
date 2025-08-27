980e67500346ec85aecaaf2a45c5a2eb7e32c4de
Shubham Mohapatra <shubhammohapatra094@gmail.com>
2025-08-28 00:03:47 +0530

    Add render.yaml for better deployment configuration

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pydantic_models import QueryInput, QueryResponse, DocumentInfo, DeleteFileR
 from langchain_utils import get_rag_chain
 from db_utils import insert_application_logs, get_chat_history, get_all_documents, insert_document_record, delete_document_record
 from chroma_utils import index_document_to_chroma, delete_doc_from_chroma
-from chroma_utils import index_document_to_chroma, delete_doc_from_chroma
 import os
 import uuid
 import logging
